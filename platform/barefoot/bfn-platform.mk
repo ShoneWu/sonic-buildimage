c19213ffe617f866d9260a23b249c8f925efe259
Weichen Chen <weichen_chen@accton.com>
2022-08-25 18:02:17 +0800

    [csp7551] Add prebuilt Barefoot SDE 9.9.0 deb for Debian11.
    [Problem description]
    Add prebuilt Barefoot SDE 9.9.0 deb for Debian11.
    
    [Root Cause]
    N/A
    
    [Affected Function]
    N/A
    
    [Resolution or Explanation]
    syncd
    
    [Modified Files]
    new file:   files/pre_build_deb/bfnplatform_1.0.0_amd64.deb
    new file:   files/pre_build_deb/bfnsdk_1.0.0_amd64.deb
    modified:   platform/barefoot/bfn-platform.mk
    modified:   platform/barefoot/bfn-sai.mk
    
    [Regression Case]
    N/A
    
    [Test Result]
    OK

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,5 +1,9 @@
-BFN_PLATFORM = bfnplatform_20220408_sai_1.9.1_deb10.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
+#BFN_PLATFORM = bfnplatform_20220815_sai_1.10_deb11.deb
+#$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
+BFN_PLATFORM = bfnplatform_1.0.0_amd64.deb
 
-SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
+#SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
+#SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
+$(BFN_PLATFORM)_PATH = files/pre_build_deb
+SONIC_COPY_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
